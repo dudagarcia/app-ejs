fd3cdda8a6d355df855174fb45b828a55ac519c2
Maria Eduarda Rodrigues Garcia <mariaedrgarcia@gmail.com>
2020-12-16 00:46:48 -0300

    CodeRecoveryScreen correction 1

--- constants/images.js
+++ constants/images.js
@@ -1,23 +1,25 @@
 export default {
+  logo: {
+    uri: require("../assets/sinteseLogo.png"),
+  },
 
-    logo:{
-        uri: require('../assets/sinteseLogo.png')
-    },
+  simonSmile: {
+    uri: require("../assets/simon01.png"),
+  },
 
-    simonSmile:{
-        uri: require('../assets/simon01.png')
-    },
+  email: {
+    uri: require("../assets/email.png"),
+  },
 
-    email:{
-        uri: require('../assets/email.png')
-    },
+  cadeado: {
+    uri: require("../assets/cadeado.png"),
+  },
 
-    cadeado:{
-        uri: require('../assets/cadeado.png')
-    },
+  simonMicrophone: {
+    uri: require("../assets/simonMicrofone.png"),
+  },
 
-    simonMicrophone:{
-        uri: require('../assets/simonMicrofone.png')
-    },
-
+  simonAmazed: {
+    uri: require("../assets/simonAmazed.png"),
+  },
 };
