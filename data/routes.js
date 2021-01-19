d244995cfe2320c358ab3d3bf781a298896ad12e
Maria Eduarda Rodrigues Garcia <mariaedrgarcia@gmail.com>
2021-01-19 01:02:08 -0300

    Login front corrections + routes.js + useState EmailCode

---
+++ data/routes.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+const connection = mysql.createPool({
+    host: 'localhost',
+    user: 'me',
+    password: 'secret',
+    database: 'my_db'
+})
+
+const app = express();
+
+app.get('/users', function (req, res) {
+    // Connecting to the database.
+    connection.getConnection(function (err, connection) {
+
+    // Executing the MySQL query (select all data from the 'users' table).
+    connection.query('SELECT * FROM users', function (error, results, fields) {
+      // If some error occurs, we throw an error.
+      //if (error) throw error;
+
+      // Getting the 'response' from the database and sending it to our route. This is were the data is.
+      res.send(results)
+    });
+  });
+});
+
+// Starting our server.
+app.listen(3000, () => {
+ console.log('Go to http://localhost:3000/users so you can see the data.');
+});
+
+//https://dev.to/saulojoab/how-to-get-data-from-an-mysql-database-in-react-native-53a4
